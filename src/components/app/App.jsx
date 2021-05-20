057a33f82e308c8f7c46945c2da3b76406e4c78f
Kat Sauma <katsauma@gmail.com>
2021-05-19 17:18:22 -0700

    Router setup

---
+++ src/components/app/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import DragContainer from '../../containers/DragContainer';
+import DetailContainer from '../../containers/DetailContainer';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component = {DragContainer} />
+        <Route exact path='/:id' component = {DetailContainer} />
+
+        <Route />
+      </Switch>
+    </Router>
+  );
+}
