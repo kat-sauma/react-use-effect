fd69ba0629516054e28d00c5a58fb369d1e08490
Kat Sauma <katsauma@gmail.com>
2021-05-20 21:21:39 -0700

    pagination and useEffect refactor

--- src/containers/DragContainer.jsx
+++ src/containers/DragContainer.jsx
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DragList from '../components/app/dragRace/DragList';
 import { fetchQueens } from '../services/DragService';
 
 
-export default class DragContainer extends Component {
-    state = {
-      loading: true,
-      queens: []   
-    };
+const DragContainer = () => {
+  const [loading, setLoading] = useState(true);
+  const [queens, setQueens] = useState([]);
+  const [page, setPage] = useState(1);
+  
+  useEffect(() => {
+    fetchQueens(page)
+    .then(setQueens)
+    .finally(() => setLoading(false));
+  }, [page]);
 
-    async componentDidMount() {
+  if(loading) return (
+    <section role="img" aria-label="loading">
+      <img role="spinner"
+        aria-label="loading spinner" 
+        src="loadingedit3.gif"
+      />;
+    </section>
+  );
+  else return (
+    <main>
+      <button onClick={() => setPage((prevPage) => prevPage - 1)}>
+        last
+      </button>
+      <p>{page}</p>
+      <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+        next
+      </button>
+      <DragList queens = { queens } />
+    </main>
+  );
+};
 
-      const queens = await fetchQueens();
-
-      this.setState({
-        queens,
-        loading: false
-      });
-    }
-    render() {
-      const { loading, queens } = this.state;
-     
-      if(loading) return (
-        <section role="img" aria-label="loading">
-          <img role="spinner"
-            aria-label="loading spinner" 
-            src="loadingedit3.gif"
-          />
-        </section>
-      );
-      else return <DragList queens = { queens } />;
-    }
-}
+export default DragContainer;
